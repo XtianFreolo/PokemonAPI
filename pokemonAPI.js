b5d1ea0e580c7d5126efc54951c61fd56cba3f48
XtianFreolo <f60@umbc.edu>
2025-04-09 11:18:29 -0400

    fetching pokemonAPI

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -1,4 +1,17 @@
 alert("Its working sire");
 
+const pokeUrl = "https://pokeapi.co/api/v2";
 
-
+fetch(pokeUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new error('There is no network response');
+        }
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There is an Error', error);
+    });
