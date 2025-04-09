99e3836722255f726b13fdc323d53d6470d0e63c
XtianFreolo <f60@umbc.edu>
2025-04-09 12:54:00 -0400

    Updated and added ".getElementById"to retrieve data from API

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -2,18 +2,24 @@ alert("Its working sire");
 
 const pokeUrl = "https://pokeapi.co/api/v2";
 
-fetchData();
+//fetchData();
 
 async function fetchData(){
 
     try{
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu"); 
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`); 
         
         if(!response.ok){
             throw new Error('Invalid Pokemon data');
         }
         const data = await response.json()
-        console.log(data);
+        const pokemonImage = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonImage");
+
+        imgElement.src = pokemonImage;
+        imgElement.style.display = "block";
 
     }
     catch(error){
