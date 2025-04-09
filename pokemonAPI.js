bc73e261d05e5b9c9b8f28724581939c3b375b07
XtianFreolo <f60@umbc.edu>
2025-04-09 14:52:57 -0400

    Figuriing out a way to get randomIndex

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -2,19 +2,21 @@ alert("Its working sire");
 
 const pokeUrl = "https://pokeapi.co/api/v2";
 
-//fetchData();
 
-async function fetchData(){
+async function fetchRandomPokemon(){
 
     try{
-
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`); 
+        // This shoukd get a random number from pokedex. +1 is bulbasaur
+        const randomId = Math.floor(Math.random() * 898) + 1;
+        // Should get randomId in pokeUrl
+        const response = await fetch(`${pokeUrl}/pokemon/${randomId}`); 
         
         if(!response.ok){
             throw new Error('Invalid Pokemon data');
         }
-        const data = await response.json()
+
+        const data = await response.json();
+
         const pokemonImage = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonImage");
 
@@ -22,8 +24,9 @@ async function fetchData(){
         imgElement.style.display = "block";
 
     }
-    catch(error){
+    catch(error) {
         console.log(error);
     }
 }
-
+// event listener for interactive button 
+document.getElementById("randomPokemonButton").addEventListener("click", fetchRandomPokemon);
