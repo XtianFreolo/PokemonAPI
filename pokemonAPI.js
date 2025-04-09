8f49ca3c7816b5536890c373375c5be7425d24b9
XtianFreolo <f60@umbc.edu>
2025-04-09 11:54:24 -0400

    corrected ")"

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -4,6 +4,12 @@ const pokeUrl = "https://pokeapi.co/api/v2";
 
 // Able to see data about pikachu
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then(response => {
+        // error handling 
+        if(!response.ok){
+            throw new Error('Could not find data');
+        }
+        return response.json()
+    })
+    .then(data => console.log(data.name))
     .catch(error => console.error(error));
