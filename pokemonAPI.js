35c785eaf5e2f83ec9375fec0bacec01b119e067
XtianFreolo <f60@umbc.edu>
2025-04-09 11:36:34 -0400

    I am able to get data from Pikachu

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -2,16 +2,8 @@ alert("Its working sire");
 
 const pokeUrl = "https://pokeapi.co/api/v2";
 
-fetch(pokeUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new error('There is no network response');
-        }
-        return response.json();
-    })
-    .then(data =>{
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('There is an Error', error);
-    });
+// Able to see data about pikachu
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
